7c970a2a31d042a9c5d690964fe7278ef439fb3f
ESWAR CHAND <chandu71202@gmail.com>
2023-09-13 17:07:57 +0530

    Eswar:Added routes for register and service

--- sample_bank/src/Home.js
+++ sample_bank/src/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Home.css';
+import {  useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+
+    let navigate = useNavigate();
+    const register = ()=>{
+        navigate('/register');
+    }
+
+    const service = ()=>{
+        navigate('/service');
+    }
+
+    
   return (
     <div className="Home">
       <header className="Home-header">
         <h1>NatWest Banking App</h1>
         <div className="button-container">
-          <button className="button">Register</button>
-          <button className="button">Service</button>
+          <button className="button" onClick={()=>register()}>Register</button>
+          <button className="button" onClick={()=>service()}>Service</button>
         </div>
       </header>
     </div>
