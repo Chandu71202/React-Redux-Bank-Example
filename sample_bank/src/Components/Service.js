6bc26a03d75eea4930d370c49e82573da9eaecd0
1192816 <rajeshwari1612001@gamil.com>
2023-09-14 10:40:22 +0530

    Raji:added popup and made changes in service

--- sample_bank/src/Components/Service.js
+++ sample_bank/src/Components/Service.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Service() {
+const Service = () => {
   return (
-    <div>service</div>
-  )
-}
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card p-4">
+            <h2 className="text-center mb-4">Financial Services</h2>
+            <div className="row">
+              <div className="col-md-6 mb-4">
+                <button className="btn btn-primary btn-lg btn-block">Banking</button>
+              </div>
+              <div className="col-md-6 mb-4">
+                <button className="btn btn-primary btn-lg btn-block">Investments</button>
+              </div>
+              <div className="col-md-6 mb-4">
+                <button className="btn btn-primary btn-lg btn-block">Loans</button>
+              </div>
+              <div className="col-md-6 mb-4">
+                <button className="btn btn-primary btn-lg btn-block">Fintech</button>
+              </div>
+              <div className="col-md-6 mb-4">
+                <button className="btn btn-primary btn-lg btn-block">Insurance</button>
+              </div>
+              <div className="col-md-6 mb-4">
+                <button className="btn btn-primary btn-lg btn-block">Exchanges</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
